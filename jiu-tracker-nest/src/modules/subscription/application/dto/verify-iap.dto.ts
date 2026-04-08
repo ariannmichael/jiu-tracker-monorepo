a2312a81a3cb52cfc02c20e9afb37dac915d7bc1
ariann-michael <ariannmichael@gmail.com>
2026-04-07 21:07:56 -0300

    Fix Subscription

--- jiu-tracker-nest/src/modules/subscription/application/dto/verify-iap.dto.ts
+++ jiu-tracker-nest/src/modules/subscription/application/dto/verify-iap.dto.ts
@@ -1,5 +1,10 @@
+import { IsIn, IsString } from 'class-validator';
+
 export class VerifyIapDto {
+  @IsIn(['apple', 'google'])
   platform!: 'apple' | 'google';
+
   /** iOS: base64 receipt data. Android: purchase token from the purchase object. */
+  @IsString()
   receipt!: string;
 }
